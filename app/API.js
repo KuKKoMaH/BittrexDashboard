6b2139a07e2ec6d7397dfd7d6104aa0fef6d8fb2
kukkoman@gmail.com <Kirik2991>
2018-01-10 18:47:33 +0300

    init

---
+++ app/API.js
@@ -0,0 +1,22 @@
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+import stringify from 'qs/lib/stringify';
+import config from './config';
+
+export default (url, params = {}) => {
+  const nonce = new Date().getTime();
+  const fullParams = {
+    ...params,
+    apikey: config.API_KEY,
+    nonce,
+  };
+  const fullUrl = config.API_URL + url + `?${stringify(fullParams)}`;
+  const apisign = hmacSHA512(fullUrl, config.API_SECRET);
+  const options = {
+    headers: new Headers({
+      apisign,
+      'Accept':       '*/*',
+      'content-type': 'application/x-www-form-urlencoded',
+    }),
+  };
+  return fetch(config.PROXY_URL + fullUrl, options).then(response => response.json());
+}
---
+++ app/config.js
@@ -0,0 +1,6 @@
+export default {
+  API_KEY:    '33c4f5f5d1b24ce099f1782afec0fdf1',
+  API_SECRET: '56eb239c7a494ffabbfd35f7e32b20ba',
+  PROXY_URL:  'http://85.143.218.243:1234/',
+  API_URL:    'https://bittrex.com/api/v1.1/',
+};
